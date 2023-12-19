3f5c43eef779a8bb5542aedc9ebc93ea8e3fd709
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2023-12-19 12:49:43 +0530

    fix: Cors policy bypass

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,10 +5,10 @@ export const LOGO =
   "https://img.freepik.com/free-vector/delivery-service-illustrated_23-2148505081.jpg?w=740&t=st=1695823021~exp=1695823621~hmac=c5222cd84a9699d26d7d27ab0c6e3fd6eaa096882a2211d6639cdfea3606b011";
 
 export const RES_API =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+  "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
 
 export const MENU_CARD_IMGID =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/";
