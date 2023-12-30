51691b1d7f82ce52b72ee7daddc1b50dce1b9b15
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2023-12-30 12:11:19 +0530

    style: menu page

--- src/components/OfferCard.js
+++ src/components/OfferCard.js
@@ -3,7 +3,7 @@ const OfferCard = (offerDataprop) => {
   const { header, description, couponCode } = offerData?.info;
   return (
     <div className="offer-main">
-      <div className="offer-card border border-border-color shadow-md h-[63px] w-[185px] rounded-md p-[9px] ">
+      <div className="offer-card border border-border-color shadow-md h-[68px] w-[185px] rounded-md p-[9px] ">
         <h3 className="text-[14px] font-bold mb-[3px] text-[#686b78] ">
           <i className="fa-solid fa-money-bills mr-2 text-swiggy-color  "> </i>
           {header}
